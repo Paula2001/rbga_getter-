751cd9ad0816b9032ceb9a2a97f40943cf866737
Paula George <polagorge@gmail.com>
2018-06-18 18:14:25 -0700

    sliders created

--- colorPicker.php
+++ colorPicker.php
@@ -3,15 +3,21 @@
 <title>Color Picker</title>
 <style>
 #color {
-    width: 100%;
-    height: 500px;
+    width: 200px;
+    height: 200px;
     background-color: green;
 }
 </style>
 
 </head>
 <body>
-<div onmousemove="can.get(event)" id="color"></div>
+<form>
+    <input oninput="slide.getValue('r')" min="0" max="255" id="r" type="range">
+    <input oninput="slide.getValue('b')" min="0" max="255" id="b" type="range">
+    <input oninput="slide.getValue('g')" min="0" max="255" id="g" type="range">
+    <input oninput="slide.getValue('r')" min="0.1" max="1" step="0.1" id="a" type="range">
+</form>
+<div onmousemove= "box.get(event)" id="color"></div>
 <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 
-function Canvas(){
-    this.color = window.getComputedStyle(document.getElementById("color")).getPropertyValue("background-color");
-    this.box = document.getElementById("color") ;
-    this.get = function(event){
+class ColorBox { //color area class
+    constructor(){
+        this.box = document.getElementById("color") ;
+        this.color = window.getComputedStyle(document.getElementById("color")).getPropertyValue("background-color");
+
+
+    }
+    get(event) {
         let coordinets = event.clientY ;
         console.log(coordinets / 10)
         this.box.style.backgroundColor = coordinets / 2 ,200 ,64, 64;
     }
+}
+class Slider extends ColorBox { //slider class
+    constructor(){
+        super();
+        this.r = document.getElementById("r");
+        this.b = document.getElementById("b");
+        this.g = document.getElementById("g");
+        this.a = document.getElementById("a");
+
+    }
+    getValue(){
+        this.box.style.backgroundColor = "rgba(" + [this.r.value,this.b.value,this.g.value,this.a.value].join(',') +")";
 
 
+    }
+   /* getValue(ele){
+        this.element = document.getElementById(`${ele}`);
+        console.log(this.element.value);
+        if (ele === "r") {}
+        this.box.style.backgroundColor = "rgba(" + [this.element.value,this.element.value,100,2].join(',') +")";
 
-};
-let can = new Canvas();
+    }*/
+}
+let slide = new Slider();
+let box = new ColorBox();
 
 
 
